df4c9a16f09e88a28322e13a9696ab5f6aa322bb
thaianh4780 <100453973+thaianh4780@users.noreply.github.com>
2022-07-19 19:28:41 +0700

    all code

--- App.tsx
+++ App.tsx
@@ -1,22 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, { useEffect, useState } from 'react'
+import { Text, View } from 'react-native'
+import SelectBox, { Item } from 'react-native-multi-selectbox-typescript'
+import axios, { AxiosResponse } from 'axios'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+// Options data must contain 'item' & 'id' keys
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+type dataAPI = {
+  code: string,
+  name: string,
 }
+
+type responseAPI = {
+  results: dataAPI[]
+}
+
+const APIProvince = 'https://api.mysupership.vn/v1/partner/areas/province';
+const APIDistrist = 'https://api.mysupership.vn/v1/partner/areas/district';
+const APICommune = 'https://api.mysupership.vn/v1/partner/areas/commune';
+
+function App() {
+  const [province, setProvince] = useState<Item>()
+  const [provinces, setProvinces] = useState<Item[]>()
+  const [district, setDistrict] = useState<Item>()
+  const [districts, setDistricts] = useState<Item[]>()
+  const [commune, setCommune] = useState<Item>()
+  const [communes, setCommunes] = useState<Item[]>()
+
+  useEffect(() => {
+    axios.get<responseAPI>(APIProvince).then((response: AxiosResponse<responseAPI>) => {
+      let tempResult: Item[] = response.data.results.map((result) => {
+        return {
+          item: result.name,
+          id: result.code
+        }
+      });
+      setProvinces(tempResult);
+    })
+  }, [])
+
+  useEffect(() => {
+    setDistricts(undefined)
+    setDistrict(undefined)
+    province && axios.get<responseAPI>(APIDistrist, { params: { "province": province.id } }).then((response: AxiosResponse<responseAPI>) => {
+      let tempResult: Item[] = response.data.results.map((result) => {
+        return {
+          item: result.name,
+          id: result.code
+        }
+      });
+      setDistricts(tempResult);
+    })
+  }, [province])
+
+  useEffect(() => {
+    setCommunes(undefined)
+    setCommune(undefined)
+    district && axios.get<responseAPI>(APIDistrist, { params: { "district": district.id } }).then((response: AxiosResponse<responseAPI>) => {
+      let tempResult: Item[] = response.data.results.map((result) => {
+        return {
+          item: result.name,
+          id: result.code
+        }
+      });
+      setDistricts(tempResult);
+    })
+  }, [district])
+
+
+  return (
+    <View style={{ margin: 30 }}>
+      <View style={{ width: '100%', alignItems: 'center' }}>
+        <Text style={{ fontSize: 30, paddingBottom: 20 }}>Demos</Text>
+      </View>
+      <Text style={{ fontSize: 20, paddingBottom: 10 }}>Select Demo</Text>
+      <SelectBox
+        label="Select single"
+        options={provinces}
+        value={province}
+        onChange={(val: Item) => setProvince(val)}
+        hideInputFilter={true}
+      />
+      <SelectBox
+        label="Select district"
+        options={districts}
+        value={district}
+        onChange={(val: Item) => setDistrict(val)}
+        hideInputFilter={true}
+      />
+      <SelectBox
+        label="Select commune"
+        options={communes}
+        value={commune}
+        onChange={(val: Item) => setCommune(val)}
+        hideInputFilter={true}
+      />
+
+
+
+
+
+
+
+
+
+
+
+
+    </View>
+
+  )
+
+}
+
+export default App
